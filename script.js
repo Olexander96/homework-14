ec9e366ef92a878a58c84bff7d244032b7ec249d
Olexander96 <o.sheiko96@gmail.com>
2022-08-30 21:13:25 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+// 1)
+    /*let a = 0.1;
+    let b = 0.2;
+    let sum = a + b;
+    console.log(sum.toFixed(1))*/
+
+// 2)
+    /*let a = "1";
+    let b = 2;
+    let sum = +a + b;
+    console.log(sum)*/
+
+// 3)
+    /*let gb = +prompt('Вкажіть об\'єм пам\'яті на флешці', 1);
+    let mb = gb * 1000;
+    let files = Math.floor(mb / 820);
+    alert(`На флешці буде місце для ${files} файлів`);*/
+
+// 4)
+    /*let money = +prompt('Введіть суму, в грн.', 0);
+    let prise = +prompt('Введіть ціну шоколадки, в грн.', 0);
+    let quatity = Math.floor(money / prise);
+    let moneyInEnd = money % prise;
+    alert(`Ви зможете купити ${quatity} шоколадок, залишок ${moneyInEnd} грн.`)*/
+
+// 5)
+    /* let number = prompt('Введіть трьохзначне число', 000);
+    let numberReverse = number[2] + number[1] + number[0];
+    alert(numberReverse)*/
+
+// 6)
+      /*let money = +prompt('Введіть сумму на 2 місяці', 0);
+      let interestForYear = 5;
+      let interestForMonth = (2 * interestForYear) / 12;
+      let moneyAfter = money + (money * (interestForMonth / 100));
+      alert(`Нараховано у відсотках ${interestForMonth.toFixed(2)}%, сума після вкладу ${moneyAfter.toFixed(1)}`);*/
+
+
+
+        
